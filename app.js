d31b1cd0f440358b49ba8ea07c1a01112722c6da
Christian Jones <Christian@christians-air.home>
2018-03-21 22:25:40 +0000

    editing api without updating test case

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.json({message: 'Hello'});
+	res.json({message: 'Hello world'});
 });
 
 app.listen(process.env.PORT || 3000);
